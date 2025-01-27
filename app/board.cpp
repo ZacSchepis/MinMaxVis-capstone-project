e285802a927e5808c977280c0d1520c449f5277b
Zackary Schepis <zschepis03@gmail.com>
2025-01-27 14:59:22 -0700

    rough board generalized code

--- app/board.cpp
+++ app/board.cpp
@@ -1,5 +1,25 @@
 #include "board.h"
 
-Board::Board(QWidget *parent)
-    : QWidget{parent}
-{}
+Board::Board(QWidget *parent, int r, int c)
+    : QWidget{parent}, rows(r), cols(c)
+{
+    int board_state[rows][cols];
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            board_state[i][j] = -1;
+        }
+    }
+
+}
+int Board::place(int r, int c, PieceType piece){
+    if((r >= rows || r < 0) || (c >= cols || c < 0)) return -1;
+    board_state[r][c] = piece;
+    return 0;
+}
+
+
+// This method will override the paint event
+// you guys can use this when drawing the board
+void Board::paintEvent(QPaintEvent* event){
+
+}
--- app/board.h
+++ app/board.h
@@ -2,12 +2,31 @@
 #define BOARD_H
 
 #include <QWidget>
+// We could override this and map it over
+// to something else to display the piece
+// TTT - X/O paintings
+// Che - red/black circle paintings
+enum PieceType {
+    empty_space,
+    P1,
+    P2
+};
+
 
 class Board : public QWidget
 {
     Q_OBJECT
+private:
+    int rows, cols;
+    int** board_state;
 public:
-    explicit Board(QWidget *parent = nullptr);
+    explicit Board(QWidget *parent = nullptr, int r = 3, int c = 3);
+    QWidget draw_board();
+    int place(int r, int c, PieceType piece);
+    virtual void map_piece(PieceType p_type);
+    // int place(int p, int piece);
+protected:
+    void paintEvent(QPaintEvent* event) override;
 
 signals:
 };
