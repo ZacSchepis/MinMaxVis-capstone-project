dec841588635a81c64a941a720b00b02d49f3bf7
Zackary Schepis <zschepis03@gmail.com>
2025-01-27 13:40:54 -0700

    Add board cpp/h files

---
+++ app/board.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <QWidget>
+
+class Board : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Board(QWidget *parent = nullptr);
+
+signals:
+};
+
+#endif // BOARD_H
