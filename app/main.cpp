8eb54e8ad43a486b8a8840dca5b21b4310b97139
Zackary Schepis <zschepis03@gmail.com>
2025-02-13 20:49:59 -0700

    cleared messy checkers test code

--- app/main.cpp
+++ app/main.cpp
@@ -24,34 +24,11 @@ int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     MainWindow window;
     window.setWindowTitle("MinMax Visualizers");
-<<<<<<< HEAD
-    Checkers board_game(&window);
-    // SampleBoard board_game(&window);
-    // Placing P1 and P2 pieces on a TTT board
-//    board_game.place(1,0, P1);
-//    board_game.place(1,2, P2);
-//     This will set a space to be empty
-    // board_game.place(2,2, empty_state);
-    board_game.populate_board();
-=======
-
     TicTacToe board_game(&window);
     TreeGraphics treeWidget;
     treeWidget.show();
->>>>>>> origin/main
 
     window.setCentralWidget(&board_game);
-    int c_ = 5;
-//    board_game.take
-//    QTimer::singleShot(5000, &board_game, [=]{
-//        board_game.
-//    });
-    board_game.turns(1000);
-
-//    time
-//    while(true){
-//        board_game.take_turn();
-//    }
     window.show();
 
     return app.exec();
